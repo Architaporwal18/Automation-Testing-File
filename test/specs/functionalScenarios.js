d780520693d5e6db6cf01fcb84736ad5e138c7b4
Architaporwal18 <145278068+Architaporwal18@users.noreply.github.com>
2026-01-28 12:14:40 +0530

    Update functionalScenarios.js

--- test/specs/functionalScenarios.js
+++ test/specs/functionalScenarios.js
@@ -10,11 +10,20 @@ describe('Functional Testing on Application',()=>{
         // await browser.pause(3000)
 
         await browser.url("https://only-testing-blog.blogspot.com/2014/09/selectable.html")
-        await $("button").doubleClick()
-        console.log(await browser.isAlertOpen())
-        // expectChai(await browser.isAlertOpen()).to.be.true
-        expectChai(await browser.getAlertText()).to.equal("You double clicked me.. Thank You..")
-        await browser.acceptAlert()
-        await browser.pause(3000)
+        // await $("button").doubleClick()
+        // console.log(await browser.isAlertOpen())
+        // // expectChai(await browser.isAlertOpen()).to.be.true
+        // expectChai(await browser.getAlertText()).to.equal("You double clicked me.. Thank You..")
+        // await browser.acceptAlert()
+        // await browser.pause(3000)
+        await browser.waitUntil(async () => await browser.isAlertOpen(), {
+            timeout: 5000,
+            timeoutMsg: 'Alert did not appear'
+        });
+
+        const text = await browser.getAlertText();
+        await browser.acceptAlert();
+        console.log(text);
+
     })
 })
